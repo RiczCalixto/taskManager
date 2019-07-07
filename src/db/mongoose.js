658bc0d4ab25b7eb56c069e8e40da4c290434371
Ricardo Calixto <rcalixtos@gmail.com>
2019-07-07 13:59:31 -0300

    App de Task Manager.

---
+++ src/db/mongoose.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+
+const User = mongoose.model("User", {
+  name: {
+    type: String
+  },
+  age: { type: Number }
+});
+
+const me = new User({ name: "RicZord", age: 30 });
+
+// me.save()
+//   .then(me => {
+//     console.log(me);
+//   })
+//   .catch(error => {
+//     console.log("Error!", error);
+//   });
+
+const Task = mongoose.model("Task", {
+  description: {
+    type: String
+  },
+  completed: {
+    type: Boolean
+  }
+});
+
+const task = new Task({
+  description: "NodeJS - Andrew Mead",
+  completed: false
+});
+
+// task
+//   .save()
+//   .then(() => {
+//     console.log(task);
+//   })
+//   .catch(error => {
+//     console.log("Error!", error);
+//   });
